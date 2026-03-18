cbff7f55ed7c1af2f06528bf8352d17ff0c6f0d7
Ma7moudEGY <mahmoudfatahalla@gmail.com>
2026-03-18 20:44:37 +0200

    feat: adding notes controller and note view

---
+++ controllers/note.php
@@ -0,0 +1,11 @@
+<?php
+
+$heading = "Note";
+
+$config = require("config.php");
+
+$db = new Database($config['database']);
+
+$note = $db->query('select * from notes where id = ?', [$_GET['id']])->fetch();
+
+require "views/note.view.php";
